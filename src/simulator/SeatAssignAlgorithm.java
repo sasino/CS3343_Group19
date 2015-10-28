d9e5bbcf38515a9ef0a4eaf13f5409d736941ce7
sasino <ononkkp@gmail.com>
2015-10-28 17:00:16 +0800

    Update SeatAssignAlgorithm.java

--- src/simulator/SeatAssignAlgorithm.java
+++ src/simulator/SeatAssignAlgorithm.java
@@ -1,5 +1,15 @@
 package simulator;
 
+import java.util.ArrayList;
+
 public class SeatAssignAlgorithm {
+  public static void allowSeatChange(CustomerGroup customer,Boolean action ,ArrayList<Table> allTables)
+  {
+    
+  }
   
+  public static void noSeatChange(CustomerGroup customer,Boolean action ,ArrayList<Table> allTables)
+  {
+    
+  }
 }
